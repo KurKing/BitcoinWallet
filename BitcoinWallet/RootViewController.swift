dbfa3c4d264ae11212f4cc6c764f589efe3eb5c5
Kurkin Oleksii <oleksii.kurkin@airslate.com>
2025-08-21 22:33:22 +0300

    renames

--- BitcoinWallet/RootViewController.swift
+++ BitcoinWallet/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  BitcoinWallet
 //
 //  Created by Oleksii on 21.08.2025.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         
